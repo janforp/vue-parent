c0e85b012aace3e2a35f7396485f64d897c16a7d
hspcadmin <880902>
2018-07-31 14:45:55 +0800

    VUE的组件的复用

--- vue-component/src/components/Index.vue
+++ vue-component/src/components/Index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <div>{{msg}}</div>
+    <HeaderComponent></HeaderComponent>
+    <router-view></router-view>
+    <div>我是底部</div>
   </div>
 </template>
 
 <script>
+  import HeaderComponent from './common/Header.vue'
   export default {
     name: 'Index',
+    components:{
+      "HeaderComponent":HeaderComponent
+    },
     data () {
       return {
         msg: '我是主页'
---
+++ vue-component/src/components/common/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>我是头部组件</div>
+  </div>
+</template>
+<script>
+ //组件中可以有自己的请求，相当于一个普通的vue,有自己的使命周期
+</script>
