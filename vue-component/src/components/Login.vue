79fc369778a49550a0faafcc9f8fbe9517ad4e44
hspcadmin <880902>
2018-07-31 15:25:26 +0800

    3.子组件向父组件传参数

---
+++ vue-component/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <LoginDiv @transferUser="getUser"></LoginDiv>
+    <p>父组件收到的用户名为：{{user}}</p>
+  </div>
+</template>
+<script>
+  import LoginDiv from './common/Input.vue'
+  export default{
+      name:'Login',
+      data() {
+          return {
+              user:''
+          }
+      },
+      methods:{
+          getUser(msg) {
+              this.user = msg
+          }
+      },
+      components:{
+          LoginDiv
+      }
+  }
+</script>
---
+++ vue-component/src/components/common/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div>
+     <label>
+       <span>子组件输入的用户名：</span>
+       <input v-model="username" @change="setUser"/>
+     </label>
+    </div>
+  </section>
+</template>
+<script>
+  export default{
+      name:'Input',
+      data() {
+          return {
+              username: ''
+          }
+      },
+      methods:{
+          setUser:function () {
+            this.$emit('transferUser', this.username)
+          }
+      }
+  }
+</script>
