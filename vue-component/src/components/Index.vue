c178d3455e4525ec0224c5846ff39ebeccb4a61f
hspcadmin <880902>
2018-07-31 14:36:06 +0800

    VUE的组件的复用

---
+++ vue-component/src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        msg: '我是主页'
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
