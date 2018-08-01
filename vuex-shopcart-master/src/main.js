ec61e642bb049a639b26435b5a88a81159f99abc
hspcadmin <880902>
2018-08-01 09:57:21 +0800

    vuex

--- vuex-shopcart-master/src/main.js
+++ vuex-shopcart-master/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import App from './App';
+
+//引入store----vuex
 import store from './store'
 
 new Vue({
     el: '#app',
+
+    //把 vuex 引入vue中
     store,
     template: '<App/>',
     components: {
